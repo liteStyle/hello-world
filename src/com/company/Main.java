81c577ceb2f34e51ce9f6961d5d4b13a8d83530c
Stanislav <xxxlitexxx@gmail.com>
2020-07-14 15:40:06 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.awt.color.ICC_Profile;
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
